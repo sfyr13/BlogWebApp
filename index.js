178f918f041749ac942bbe45dac4494c7636f0af
Μάνος Σφυράκης <sfyr131@gmail.com>
2024-10-07 12:46:30 +0300

    Update newPost.ejs

--- index.js
+++ index.js
@@ -20,7 +20,26 @@ app.get("/new", (req, res) => {
 
 });
 
+app.post("/new", (req, res) => {
+    const {title, content} = req.body;
 
+    // Add the new post to the in-memory storage
+    const newPost = {
+        title: title,
+        content: content,
+        createdAt: new Date().toLocaleString(),
+};
+
+    posts.push(newPost);
+
+    // Redirect back to the home page to view all posts
+    res.redirect('/');
+});
+
+app.get("/my-posts", (req, res) => {
+    res.render("userPosts.ejs", {posts : posts});
+
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
